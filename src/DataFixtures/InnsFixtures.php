f1b2f9afc2aa09791586ee9d4c1e3cd9507eaf72
Albert Mełges <albert.melges@gmail.com>
2020-06-26 23:06:08 +0200

    feat(DataFixtures): added stub for all data fixturesj

---
+++ src/DataFixtures/InnsFixtures.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class InnsFixtures extends AbstractBaseFixtures
+{
+    /**
+     * @inheritDoc
+     */
+    protected function loadData(ObjectManager $manager): void
+    {
+        // TODO: Implement loadData() method.
+    }
+}
