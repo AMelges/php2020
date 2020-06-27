fd8c87835fed79b0d40bb96048f00b15eee613b2
Albert Mełges <albert.melges@gmail.com>
2020-06-27 02:58:24 +0200

    feat(User): updated User with security options

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,15 +3,57 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
- * User
+ * User.
  *
- * @ORM\Table(name="users", uniqueConstraints={@ORM\UniqueConstraint(name="ID_UNIQUE", columns={"ID"}), @ORM\UniqueConstraint(name="login_UNIQUE", columns={"login"}), @ORM\UniqueConstraint(name="email_UNIQUE", columns={"email"})})
+ * @ORM\Table(
+ *     name="users",
+ *     uniqueConstraints={
+ *     @ORM\UniqueConstraint(name="ID_UNIQUE", columns={"ID"}),
+ *     @ORM\UniqueConstraint(name="login_UNIQUE", columns={"login"}),
+ *     @ORM\UniqueConstraint(name="email_UNIQUE", columns={"email"})})
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ * @UniqueEntity(fields={"email"})
  */
 class User
 {
+    /**
+     * Role admin.
+     *
+     * @var string
+     */
+    const ROLE_ADMIN = 'ROLE_ADMIN';
+
+    /**
+     * Role king.
+     *
+     * @var string
+     */
+    const ROLE_KING = 'ROLE_KING';
+
+    /**
+     * Role chieftain.
+     *
+     * @var string
+     */
+    const ROLE_CHIEFTAIN = 'ROLE_CHIEFTAIN';
+
+    /**
+     * Role inkeeper.
+     *
+     * @var string
+     */
+    const ROLE_INNKEEPER = 'ROLE_INNKEEPER';
+
+    /**
+     * Role standard.
+     *
+     * @var string
+     */
+    const ROLE_STANDARD = 'ROLE_STANDARD';
+
     /**
      * @var int
      *
@@ -43,22 +85,24 @@ class User
     private $email;
 
     /**
-     * @var string|null
+     * Roles.
      *
-     * @ORM\Column(name="roles", type="text", length=0, nullable=true)
+     * @ORM\Column(type="json")
      */
-    private $roles;
+    private $roles = [];
 
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
-
+    /**
+     * @return string|null
+     */
     public function getLogin(): ?string
     {
         return $this->login;
     }
 
+    /**
+     * @param string|null $login
+     * @return $this
+     */
     public function setLogin(?string $login): self
     {
         $this->login = $login;
@@ -66,41 +110,110 @@ class User
         return $this;
     }
 
-    public function getPassword(): ?string
+    /**
+     * Getter for the Id.
+     *
+     * @return int|null Result
+     */
+    public function getId(): ?int
     {
-        return $this->password;
+        return $this->id;
     }
 
-    public function setPassword(?string $password): self
-    {
-        $this->password = $password;
-
-        return $this;
-    }
-
+    /**
+     * Getter for the E-mail.
+     *
+     * @return string|null E-mail
+     */
     public function getEmail(): ?string
     {
         return $this->email;
     }
 
-    public function setEmail(?string $email): self
+    /**
+     * Setter for the E-mail.
+     *
+     * @param string $email E-mail
+     */
+    public function setEmail(string $email): void
     {
         $this->email = $email;
-
-        return $this;
     }
 
-    public function getRoles(): ?string
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     *
+     * @return string User name
+     */
+    public function getUsername(): string
     {
-        return $this->roles;
+        return (string) $this->email;
     }
 
-    public function setRoles(?string $roles): self
+    /**
+     * Getter for the Roles.
+     *
+     * @see UserInterface
+     *
+     * @return array Roles
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_STANDARD
+        $roles[] = static::ROLE_STANDARD;
+
+        return array_unique($roles);
+    }
+
+    /**
+     * Setter for the Roles.
+     *
+     * @param array $roles Roles
+     */
+    public function setRoles(array $roles): void
     {
         $this->roles = $roles;
-
-        return $this;
     }
 
+    /**
+     * Getter for the Password.
+     *
+     * @see UserInterface
+     *
+     * @return string|null Password
+     */
+    public function getPassword(): string
+    {
+        return (string) $this->password;
+    }
 
+    /**
+     * Setter for the Password.
+     *
+     * @param string $password Password
+     */
+    public function setPassword(string $password): void
+    {
+        $this->password = $password;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getSalt()
+    {
+        // not needed when using the "bcrypt" algorithm in security.yaml
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials()
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
 }
